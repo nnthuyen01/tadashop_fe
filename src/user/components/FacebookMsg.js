e42a2806f9bae1903b626a330af90fc7e98d72cd
Thuyen <tmdthuyen@gmail.com>
2023-11-22 11:56:36 +0700

    chat messenger plugin

---
+++ src/user/components/FacebookMsg.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { FacebookProvider, CustomChat } from 'react-facebook';
+
+export default class FacebookMsg extends Component {
+    render() {
+        return (
+            <FacebookProvider appId="363776702683907" chatSupport>
+                <CustomChat pageId="177460258781408" minimized={true} />
+            </FacebookProvider>
+        );
+    }
+}
--- src/user/layouts/DefaultLayout/DefaultLayout.js
+++ src/user/layouts/DefaultLayout/DefaultLayout.js
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import ScrollToTopButton from '~/user/components/ScrollToTopButton';
+import FacebookMsg from '~/user/components/FacebookMsg';
 
 function DefaultLayout({ children }) {
     return (
         <Fragment>
             {/* <Header /> */}
             {children}
+            <FacebookMsg />
             <ScrollToTopButton />
             <Footer />
         </Fragment>
